55ad4711498241b8237c6c2e47a672dce4761848
Shruti <shrutishinde736@gmail.com>
2023-08-20 15:25:01 +0530

    edie homepage added

---
+++ edie-homepage/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat",
+        heebo: "Heebo",
+        poppins: "Poppins",
+      },
+      colors: {
+        gray1: "#333333",
+        blue: "#2D9CDB",
+        gray2: "#4F4F4F",
+        gray3: "#828282",
+        gray4: "#BDBDBD",
+        gray5: "#E0E0E0",
+        gray6: "#F2F2F2",
+        red: "#EB5757",
+        darkBlue: "#100E1D",
+        green: "#27AE60",
+      },
+    },
+  },
+  plugins: [],
+};
